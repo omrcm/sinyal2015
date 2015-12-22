917c7f174ee6654cf77336b51b27e562d8b9320c
omrcm <omurucum@gmail.com>
2015-12-22 02:55:57 +0200

    Update note.m

--- note.m
+++ note.m
@@ -1,1 +1,6 @@
 
+function [x] = note( lover, higher )  %x bir değer dönderen ve 2 parametre alan bir note isimli fonksiyon.
+  t=lover:(higher-lover)/100:higher;%higher ve lover arasında değişen bir sınır.
+  x=sin(t);%x sinüs sinyali istendiği için
+  plot(x);%oluşturulan sinyalin grafiği
+end
